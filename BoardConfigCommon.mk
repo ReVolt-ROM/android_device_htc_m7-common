e04134779d061e0ec83149f54ad58cfa9eb4f9e2
Ethan Chen <intervigil@gmail.com>
2013-06-11 14:56:26 -0700

    m7-common: move common defines up to msm8960-common
    Change-Id: I1f8a2ef83bd8e8ecf0e928b8c2ff1be3b059641c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,14 +51,12 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 #TARGET_GCC_VERSION := 4.6
 
 # Flags
-COMMON_GLOBAL_CFLAGS += -DNEW_ION_API=1
 COMMON_GLOBAL_CFLAGS += -DNEW_LIBRIL_HTC
 
 # Audio
 BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
 BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recog/camcorder use cases
 BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
-TARGET_USES_QCOM_COMPRESSED_AUDIO := true
 BOARD_HAVE_HTC_CSDCLIENT := true
 
 # Bluetooth
